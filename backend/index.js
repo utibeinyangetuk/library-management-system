bee9419e830226c09b827b4446a7108e91ff6777
utibe inyangetuk <inyangutibz@gmail.com>
2023-03-01 01:01:20 +0100

    fixing git

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello utibe!'))
 app.listen(port, () => console.log(`server running on port ${port}!`))
